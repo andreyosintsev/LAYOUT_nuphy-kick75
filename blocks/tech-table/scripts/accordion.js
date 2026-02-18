eaaf0a8d407814e98a4301f0ad09c12940147017
andreyosintsev <kintaro_oe@inbox.ru>
2026-02-18 19:11:32 +0400

    feat: block tech-table added

---
+++ blocks/tech-table/scripts/accordion.js
@@ -0,0 +1,28 @@
+(function(){
+
+    document.addEventListener('DOMContentLoaded', () => {
+
+        const accordions = document.querySelectorAll('.accordion');
+
+        accordions.forEach(accordion => {
+            const accordionItemTitles = accordion.querySelectorAll('.accordion__item-title');
+
+            accordionItemTitles.forEach(accordionItemTitle =>
+                accordionItemTitle.addEventListener('click', function() {
+                        const item = this.closest('.accordion__item');
+                        const content = item.querySelector('.accordion__item-content');
+
+                        if (item.classList.contains('accordion__item_active')) {
+                            content.style.maxHeight = '0';
+                            item.classList.remove('accordion__item_active');
+                        } else {
+                            item.classList.add('accordion__item_active');
+                            content.style.maxHeight = content.scrollHeight + 'px'; //
+                        }
+                    }
+                )
+            );
+        })
+    });
+
+})();
